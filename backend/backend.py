c8339ddefe4f2e6a6a95c695265fdf93189124fe
Luke H Miles <luke@mm.st>
2020-03-20 03:19:38 -0500

    made backend the tweepy hello world

--- backend/backend.py
+++ backend/backend.py
@@ -1,1 +1,15 @@
-# maybe write backend in python for using ML/NLP stuff?
+import os
+import tweepy
+
+auth = tweepy.OAuthHandler(
+    os.environ.get("consumer_key"), os.environ.get("consumer_secret")
+)
+auth.set_access_token(
+    os.environ.get("access_token"), os.environ.get("access_token_secret")
+)
+
+api = tweepy.API(auth)
+
+public_tweets = api.home_timeline()
+for tweet in public_tweets:
+    print(tweet.text)
