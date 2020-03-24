c058aa65b7ff049b05d9c488f64c942c955c066a
Luke H Miles <luke@mm.st>
2020-03-24 13:27:54 -0700

    grabbing cursor from scroll

--- js/background.js
+++ js/background.js
@@ -75,43 +75,70 @@ function onInstalled() {
   });
 }
 
-function getUrlForTweetId(tweetId) {
-  let params = new URLSearchParams({
-    include_reply_count: "1",
-    tweet_mode: "extended"
-  });
-  // if (cursor !== null && cursor !== undefined) {
-  //   params.set("cursor", cursor);
-  // }
-  return `https://api.twitter.com/2/timeline/conversation/${tweetId}.json?${params.toString()}`;
-}
-
-function fetchTweets(tweetId, auth) {
-  let url = getUrlForTweetId(tweetId);
-  let fetch = typeof content === "undefined" ? window.fetch : content.fetch;
-  return fetch(url, {
-    credentials: "include",
-    headers: {
-      "x-csrf-token": auth.csrfToken,
-      authorization: auth.authorization
-    }
-  })
-    .then(x => x.json())
-    .catch(e => console.error("Failed to load tweets", e));
-}
-
-function fetchTrump(since, until, auth) {
-  var query = escape(
-    `from:realdonaldtrump since:${since} until:${until} include:retweets`
-  );
-  var url = `https://api.twitter.com/2/search/adaptive.json?q=${query}&count=20`;
-  var x = fetch(url, {
+function fetchTrump(
+  auth,
+  cursor = null,
+  since = "2012-01-01",
+  until = "2012-01-31"
+) {
+  const query = escape(`from:realdonaldtrump since:${since} until:${until}`);
+  let url = `https://api.twitter.com/2/search/adaptive.json?q=${query}&count=20`;
+  if (cursor !== null) {
+    url += `&cursor=${escape(cursor)}`;
+  }
+  console.log("fetching url: " + url);
+  const init = {
     credentials: "include",
     headers: {
       authorization: auth.authorization,
       "x-csrf-token": auth.csrfToken
     }
-  });
-  var y = x.then(v => v.json());
-  return y;
+  };
+  return fetch(url, init)
+    .then(x => x.json())
+    .catch(e => console.error("Failed to load tweets", e));
 }
+
+//** Finds 'cursor' value which corresponds to scroll in the search results */
+function parseCursor(response) {
+  for (let entry of response.timeline.instructions[0].addEntries.entries) {
+    if (entry.content.operation && entry.content.operation.cursor) {
+      if (entry.content.operation.cursor.cursorType === "Bottom") {
+        const cursor = entry.content.operation.cursor.value;
+        console.log("found cursor in response:" + cursor);
+        return cursor;
+      }
+    }
+  }
+  // return cursor.substr(7); // remove 'cursor:'
+  return null;
+}
+
+// function parseTweets(response) {
+//   let tweets = [];
+//   let users = new Map();
+//   for (let userId in response.globalObjects.users) {
+//     let user = response.globalObjects.users[userId];
+//     users.set(userId, {
+//       handle: user.screen_name,
+//       name: user.name,
+//       avatar: user.profile_image_url_https
+//     });
+//   }
+//   for (let tweetId in response.globalObjects.tweets) {
+//     let entry = response.globalObjects.tweets[tweetId];
+//     let tweet = new Tweet();
+//     let user = users.get(entry.user_id_str);
+//     tweet.id = entry.id_str;
+//     tweet.bodyText = entry.full_text;
+//     tweet.bodyHtml = entry.full_text;
+//     tweet.name = user.name;
+//     tweet.username = user.handle;
+//     tweet.avatar = user.avatar;
+//     tweet.parent = entry.in_reply_to_status_id_str;
+//     tweet.time = new Date(entry.created_at).getTime();
+//     tweet.replies = entry.reply_count;
+//     tweets.push(tweet);
+//   }
+//   return tweets;
+// }
