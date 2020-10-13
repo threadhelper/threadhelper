419b7cd364b301929c6291ff43a52f5629c04e41
TheExGenesis <theexgenesis@gmail.com>
2020-10-13 10:54:17 +0100

    run build

--- src/utils/ga.jsx
+++ src/utils/ga.jsx
@@ -1,12 +1,17 @@
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-XXXXXXXX-X']);
-_gaq.push(['_trackPageview']);
 
-(function() {
+
+const getGaq = ()=>{
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-170230545-1']);
+  _gaq.push(['_trackPageview']);
+  return _gaq
+}
+
+export function activateTracking() {
   var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
   ga.src = 'https://ssl.google-analytics.com/ga.js';
   var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-})();
+}
 
 function trackButton(e) {
   _gaq.push(['_trackEvent', e.target.id, 'clicked']);
